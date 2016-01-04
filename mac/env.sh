6164b1ff5962e3691a1a6fd08eda23d20052f2d9
Justin Page <xjustinpagex@gmail.com>
2016-01-04 11:55:27 -0800

    Updated environment config

--- mac/env.sh
+++ mac/env.sh
@@ -4,6 +4,9 @@
 BASE16_SHELL="$HOME/.config/base16-shell/base16-ocean.dark.sh"
 [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
 
+TEN="~/Projects/www/TEN/htdocs/vvv/www/ten/wordpress/wp-content"
+GRD="~/Projects/www/OffGrid/htdocs/hgv/hgv_data/sites/hhvm/wp-content"
+
 # aliases
 alias c='clear'
 alias h='cd ~'
@@ -14,8 +17,13 @@ alias x='exit'
 alias vssh='vagrant ssh'
 alias vup='vagrant up'
 alias vstat='vagrant global-status'
+alias vhalt='vagrant halt'
 alias host='sudo vim /etc/hosts'
 alias pgit='cd ~/Projects/git'
+
+alias ten="cd $TEN"
+alias offgrid="cd $GRD"
+alias lowrider="cd $TEN/themes/low-rider"
 
 # Functions
 ds () {
@@ -24,23 +32,23 @@ ds () {
 }
 # extract current compressed filed -> provide argument
 extract () {
-	    if [ -f $1 ] ; then
-			        case $1 in
-						*.tar.bz2)  tar xjf $1      ;;
-						*.tar.gz)   tar xzf $1      ;;
-						*.bz2)      bunzip2 $1      ;;
-						*.rar)      rar x $1        ;;
-						*.gz)       gunzip $1       ;;
-						*.tar)      tar xf $1       ;;
-						*.tbz2)     tar xjf $1      ;;
-						*.tgz)      tar xzf $1      ;;
-						*.zip)      unzip $1        ;;
-						*.Z)        uncompress $1   ;;
-						*)          echo "'$1' cannot be extracted via extract()" ;;
-					esac
-		else
-			echo "'$1' is not a valid file"
-		fi
+	if [ -f $1 ] ; then
+		case $1 in
+			*.tar.bz2)  tar xjf $1      ;;
+			*.tar.gz)   tar xzf $1      ;;
+			*.bz2)      bunzip2 $1      ;;
+			*.rar)      rar x $1        ;;
+			*.gz)       gunzip $1       ;;
+			*.tar)      tar xf $1       ;;
+			*.tbz2)     tar xjf $1      ;;
+			*.tgz)      tar xzf $1      ;;
+			*.zip)      unzip $1        ;;
+			*.Z)        uncompress $1   ;;
+			*)          echo "'$1' cannot be extracted via extract()" ;;
+		esac
+	else
+		echo "'$1' is not a valid file"
+	fi
 }
 
 updateBundle() {
